5db6c98ed56bfee749110542650ec5b353c22215
RongLi <rongrong.118@hotmail.com>
2013-11-13 00:16:25 -0800

    added exception handle

--- Algorithms_HW7_SeamCarver/SeamCarving/src/SeamCarver.java
+++ Algorithms_HW7_SeamCarver/SeamCarving/src/SeamCarver.java
@@ -30,6 +30,9 @@ public class SeamCarver {
    
    public double energy(int x, int y){       
    // energy of pixel at column x and row 
+	   if (x<0||y<0||x>width-1||y>height-1)  
+   		throw new IndexOutOfBoundsException();
+	   
 	   Color c0,c1,c2,c3;
 	   int rx,gx,bx,ry,gy,by;
 	   double pow_diffx,pow_diffy;
@@ -249,11 +252,15 @@ public class SeamCarver {
    
    public void removeHorizontalSeam(int[] a){
 	   // remove horizontal seam from picture
+	   if(a.length!=width) throw new IndexOutOfBoundsException();
+	   if(width<=1) throw new RuntimeException();
+	   
 	   Color[][] color_src=new Color[width][height];
 	   Color[][] color_dst=new Color[width][height-1];
 	   
 	   double energy_src[][]= new double[width][height];
 	   double energy_dst[][]= new double[width][height-1];
+	   
 	   for(int i=0;i<height;i++){
 		   for(int j=0;j<width;j++){
 			   energy_src[j][i]=energy(j,i);
@@ -289,18 +296,23 @@ public class SeamCarver {
            {
                pic.set(j, i, color_src[j][i]);
            }
-	   height=height-1;
 	   
+	   height=height-1;
    }
    
    
    public void removeVerticalSeam(int[] a){
 	   // remove vertical seam from picture
+	   
+	   if(a.length!=height) throw new IndexOutOfBoundsException();
+	   if(height<=1) throw new RuntimeException();
+	   
 	   Color[][] color_src=new Color[width][height];
 	   Color[][] color_dst=new Color[width-1][height];
 	   
 	   double energy_src[][]= new double[width][height];
 	   double energy_dst[][]= new double[width-1][height];
+	   
 	   for(int i=0;i<height;i++){
 		   for(int j=0;j<width;j++){
 			   energy_src[j][i]=energy(j,i);
@@ -336,6 +348,7 @@ public class SeamCarver {
            {
                pic.set(j, i,color_src[j][i]);
            }
+	   
 	   width=width-1;
 	   
    }
